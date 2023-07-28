05122e943c7dd6ee73498f1cfd12a96e92a62c86
manmay27 <98768586+manmay27@users.noreply.github.com>
2023-07-28 22:42:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Check from './check';
+import './App.css';
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <Check/>
+  </React.StrictMode>
+  );
+
+
